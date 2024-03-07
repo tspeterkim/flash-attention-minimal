9c1d313a8abd7fe66a063a7e155ae11bd1422a3a
Peter Kim <tspeterkim3@gmail.com>
2024-03-07 16:47:22 +0900

    simple version

---
+++ benchmark.py
@@ -0,0 +1,38 @@
+import math
+
+import torch
+from torch.nn import functional as F
+from torch.utils.cpp_extension import load
+
+# Load the CUDA kernel as a python module
+minimal_attn = load(name='minimal_attn', sources=['main.cpp', 'flash.cu'], extra_cuda_cflags=['-O2'])
+
+batch_size = 16
+n_head = 12
+seq_len = 64
+head_embd = 64
+
+q = torch.randn(batch_size, n_head, seq_len, head_embd).cuda()
+k = torch.randn(batch_size, n_head, seq_len, head_embd).cuda()
+v = torch.randn(batch_size, n_head, seq_len, head_embd).cuda()
+
+print('=== profiling manual attention ===')
+
+# Our minimal flash attention needs to be faster than this.
+def manual_attn(q, k, v):
+    att = (q @ k.transpose(-2, -1) * (1.0 / math.sqrt(k.size(-1))))
+    att = F.softmax(att, dim=-1)
+    y = att @ v
+    return y
+
+with torch.autograd.profiler.profile(use_cuda=True) as prof:
+    manual_result = manual_attn(q, k, v)
+print(prof.key_averages().table(sort_by='cuda_time_total', row_limit=10))
+
+print('=== profiling minimal flash attention === ')
+
+with torch.autograd.profiler.profile(use_cuda=True) as prof:
+    minimal_result = minimal_attn.forward(q, k, v)
+print(prof.key_averages().table(sort_by='cuda_time_total', row_limit=10))
+
+print('attn values sanity check:', torch.allclose(minimal_result, manual_result, rtol=0, atol=1e-02))
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <torch/extension.h>
+
+torch::Tensor forward(torch::Tensor q, torch::Tensor k, torch::Tensor v);
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+    m.def("forward", torch::wrap_pybind_function(forward), "forward")
+}
