b7be30586a3f54285940477dd226f2ca6c5755bf
Peter Kim <tspeterkim3@gmail.com>
2024-03-07 18:42:19 +0900

    add includes

--- flash.cu
+++ flash.cu
@@ -1,4 +1,8 @@
-__global__ 
+#include <torch/types.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+__global__
 void forward_kernel(const float* Q, const float* K, const float* V, const int N, const int d,
                     const int Tc, const int Tr, const int Bc, const int Br, const float softmax_scale,
                     float* l, float *m, float* O) {
--- main.cpp
+++ main.cpp
@@ -3,5 +3,5 @@
 torch::Tensor forward(torch::Tensor q, torch::Tensor k, torch::Tensor v);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.def("forward", torch::wrap_pybind_function(forward), "forward")
+    m.def("forward", torch::wrap_pybind_function(forward), "forward");
 }
