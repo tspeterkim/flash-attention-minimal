09507d37e9d38797fe712698c39873b75bcd6d64
Franz Louis Cesista <franzlouiscesista@gmail.com>
2024-03-19 13:46:30 +0800

    implement backward pass

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <torch/extension.h>
 
-torch::Tensor forward(torch::Tensor q, torch::Tensor k, torch::Tensor v);
+std::vector<torch::Tensor> forward(torch::Tensor Q, torch::Tensor K, torch::Tensor V);
+std::vector<torch::Tensor> backward(
+    torch::Tensor Q,
+    torch::Tensor K,
+    torch::Tensor V,
+    torch::Tensor O,
+    torch::Tensor dO,
+    torch::Tensor l,
+    torch::Tensor m
+);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("forward", torch::wrap_pybind_function(forward), "forward");
+    m.def("backward", torch::wrap_pybind_function(backward), "backward");
 }
